82b719bcc47091df7cc050e65b31b1313c8f5776
rmward17 <wardrachelm17@gmail.com>
2022-03-11 11:46:57 -0500

    commit 3

--- Week 3 - Python/3-2-Student-Resources/07-Stu_ReadNetFlixCSV/netflix.py
+++ Week 3 - Python/3-2-Student-Resources/07-Stu_ReadNetFlixCSV/netflix.py
@@ -3,20 +3,20 @@ import os
 import csv
 
 # Prompt user for video lookup
-video = input("What show or movie are you looking for? ")
+#video = input("What show or movie are you looking for? ")
 
 # Set path for file
-csvpath = os.path.join("Resources","netflix_ratings.csv")
-
+file_to_load = os.path.join("Week 3 - Python\3-2-Student-Resources\07-Stu_ReadNetFlixCSV\Resources\netflix_ratings.csv")
+print(file_to_load)
 # Open the CSV
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+#with open(file_to_load) as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
 
     # Loop through looking for the video
-    for row in csvreader:
-        if row[0] == video:
-            print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
-    else:
-        print("Sorry about this, we don't seem to have what you are looking for!")
+    #for row in csvreader:
+    #    if row[0] == video:
+    #        print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+   # else:
+    #    print("Sorry about this, we don't seem to have what you are looking for!")
 
 
