7623679b646424f072c117b09339aff2fa6d4348
rmward17 <wardrachelm17@gmail.com>
2022-04-27 21:52:42 -0400

    activity 2

--- Week10_Webscraping/10-2-Student_Resources/04-Evr_Render_List_and_Dict/Unsolved/app.py
+++ Week10_Webscraping/10-2-Student_Resources/04-Evr_Render_List_and_Dict/Unsolved/app.py
@@ -3,12 +3,19 @@ from flask import Flask, render_template
 
 # @TODO: Initialize your Flask app here
 # CODE GOES HERE
-
+app = Flask(__name__)
 # @TODO: Create a list of dictionaries
-# CODE GOES HERE
+pets = [
+    {"name": "Fido", "type": "Lab"},
+    {"name": "Spot", "type": "Dalmation"},
+    {"name": "Artie", "type": "French Terrier"}
+]
 
 # @TODO:  Create a route and view function that takes in a dictionary and renders index.html template
 # CODE GOES HERE
-
+@app.route("/")
+def index(dict):
+    return render_template('index.html')
+    
 if __name__ == "__main__":
     app.run(debug=True)
