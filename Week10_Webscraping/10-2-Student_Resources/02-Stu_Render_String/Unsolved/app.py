41125f06b626a31c22a8ed8b9e2eb5bda196a342
rmward17 <wardrachelm17@gmail.com>
2022-04-27 21:44:53 -0400

    activity 1

--- Week10_Webscraping/10-2-Student_Resources/02-Stu_Render_String/Unsolved/app.py
+++ Week10_Webscraping/10-2-Student_Resources/02-Stu_Render_String/Unsolved/app.py
@@ -2,10 +2,17 @@
 from flask import Flask, render_template
 
 # @TODO: Initialize your Flask app here
-# CODE GOES HERE
-
+app = Flask(__name__)
 # @TODO:  Create a route and view function that takes in a string and renders index.html template
-# CODE GOES HERE
+@app.route("/")
+def index():
+    return render_template("index.html", name='Amy', hobby='yoga')
+
+#def index():
+    #return render_template("index.html", name='Rachel', hobby='cooking')
+@app.route("/bonus")
+def bonus():
+    return render_template('bonus.html', name='Amy', hobby='yoga')
 
 if __name__ == "__main__":
     app.run(debug=True)
